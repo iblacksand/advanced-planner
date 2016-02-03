4e8d8ea1651994add306f2669e893cf07162d2c8
iblacksand <chess4j@gmail.com>
2016-02-02 19:16:56 -0700

    Got all established commands to be recognized and able to execute

--- Script.java
+++ Script.java
@@ -37,41 +37,36 @@ public class Script
     */
     public void readFile()
     {
+        ArrayList<String> a = new ArrayList<String>();
+        String line = null;
         try
         {
-        FileReader fr = new FileReader(path);
-        BufferedReader textReader = new BufferedReader(fr);
-        ArrayList<String> a = new ArrayList<String>();
-        for(int i =0; i<1000;i++)
-        {
-            try
+            FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);
+            for(int i = 0; i < textSize(); i++)
             {
-            String text = textReader.readLine().trim().toLowerCase();
-            if(text!="")
-            {
-                a.add(text);
+                line = br.readLine();
+                if(line != null) a.add(line.toLowerCase().trim());
+                else break;
             }
-            else
-            {
-                break;
-            }
-            }
-            catch (IOException e)
-            {
-                break;
-            }
-        }
-        textReader.close();
-        file = new String[a.size()];
-        for(int i = 0; i < a.size(); i++)
-        {
-            file[i] = a.get(i);
-        }
-        }
 
-        catch(IOException e)
-        {
-
+            file = new String[a.size()];
+            for(int i = 0; i < file.length; i++)
+            {
+                file[i] = a.get(i);
+            }
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" +
+                path + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '"
+                + path + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
         }
     }
 
@@ -179,14 +174,24 @@ public class Script
     *Gets the properties if the current line
     *@return the array of properties
     */
-    public String[] properties()
+    public String[] properties(int k)
     {
-        String line = file[curLine];
+        String line = file[k];
         ArrayList<String> a = new ArrayList<String>();
-        for(int i = 0; i < line.length() - 5; i++)
+        for(int i = 0; i < line.length() - 6; i++)
         {
             String str = line.substring(i,i+5);
-            if(str.equals("prop(")) a.add(str.substring(i+6,i+7));
+            if(str.equals("prop("))
+            {
+                String full = "";
+                for(int x = i + 5; x < line.length(); x++)
+                {
+                    String c = line.substring(x,x+1);
+                    if(!c.equals(")")) full += c;
+                    else break;
+                }
+                a.add(full);
+            }
         }
 
         for(int i = 0; i < a.size(); i++)
@@ -200,7 +205,13 @@ public class Script
                 case "autoclose":
                     a.set(i,"ac");
                     break;
+                case "f":
+                    break;
+                case "ac":
+                    break;
                 default:
+                    a.remove(i);
+                    i--;
                     break;
             }
 
@@ -218,14 +229,24 @@ public class Script
     *@param k the line to look at
     *@return the properties of the line
     */
-    public String[] properties(int k)
+    public String[] properties()
     {
-        String line = file[k];
+        String line = file[curLine];
         ArrayList<String> a = new ArrayList<String>();
-        for(int i = 0; i < line.length() - 5; i++)
+        for(int i = 0; i < line.length() - 6; i++)
         {
             String str = line.substring(i,i+5);
-            if(str.equals("prop(")) a.add(str.substring(i+6,i+7));
+            if(str.equals("prop("))
+            {
+                String full = "";
+                for(int x = i + 5; x < line.length(); x++)
+                {
+                    String c = line.substring(x,x+1);
+                    if(!c.equals(")")) full += c;
+                    else break;
+                }
+                a.add(full);
+            }
         }
 
         for(int i = 0; i < a.size(); i++)
@@ -239,7 +260,13 @@ public class Script
                 case "autoclose":
                     a.set(i,"ac");
                     break;
+                case "f":
+                    break;
+                case "ac":
+                    break;
                 default:
+                    //a.remove(i);
+                    //i--;
                     break;
             }
 
@@ -250,6 +277,138 @@ public class Script
             props[i] = a.get(i);
         }
         return props;
+    }
+
+    /**
+    * gets the objects of the current line
+    * @return the array of objects
+    */
+    public String[] object()
+    {
+        ArrayList<String> a = new ArrayList<String>();
+        String line = file[curLine];
+        int start = 0;
+        boolean startFound = false;
+        for(int i = 0; i < line.length(); i++)
+        {
+            if(line.substring(i,i+1).equals(" "))
+            {
+                start = i;
+                startFound = true;
+                break;
+            }
+        }
+
+        String str = line.substring(start + 1);
+        String build = "";
+        int lastCut = 0;
+        for(int i = 0; i < str.length(); i++)
+        {
+            String c = str.substring(i,i+1);
+            if(c.equals(" "))
+            {
+                a.add(build.trim());
+                build = "";
+                lastCut = i;
+            }
+            else build += c.trim();
+        }
+        a.add(str.substring(lastCut).trim());
+        String[] objects = new String[a.size()];
+        for(int i = 0; i < objects.length; i++)
+        {
+            objects[i] = a.get(i);
+        }
+        if(!startFound)
+        {
+            objects = new String[0];
+        }
+        return objects;
+    }
+
+    /**
+    * gets the objects in the specified line
+    * @param k the line to look at
+    * @return the array of objects
+    */
+    public String[] object(int k)
+    {
+        ArrayList<String> a = new ArrayList<String>();
+        String line = file[k];
+        int start = 0;
+        boolean startFound = false;
+        for(int i = 0; i < line.length(); i++)
+        {
+            if(line.substring(i,i+1).equals(" "))
+            {
+                start = i;
+                startFound = true;
+                break;
+            }
+        }
+
+        String str = line.substring(start + 1);
+        String build = "";
+        int lastCut = 0;
+        for(int i = 0; i < str.length(); i++)
+        {
+            String c = str.substring(i,i+1);
+            if(c.equals(" "))
+            {
+                a.add(build.trim());
+                build = "";
+                lastCut = i;
+            }
+            else build += c.trim();
+        }
+        a.add(str.substring(lastCut).trim());
+        String[] objects = new String[a.size()];
+        for(int i = 0; i < objects.length; i++)
+        {
+            objects[i] = a.get(i);
+        }
+        if(!startFound)
+        {
+            objects = new String[0];
+        }
+        return objects;
+    }
+
+    public int fileSize()
+    {
+        return file.length;
+    }
+
+    public int textSize()
+    {
+        int t = 0;
+        try
+        {
+            FileReader fr = new FileReader("main.txt");
+            BufferedReader textReader = new BufferedReader(fr);
+
+            for(int i =0; i<1000;i++)
+            {
+                String text = textReader.readLine();
+                if(text==null
+                   ||text.trim().equals(""))
+                {
+                    textReader.close();
+                    t = i;
+                }
+                else
+                {
+
+                }
+
+            }
+
+            t = -1;
+        }
+        catch(IOException e)
+        {
+        }
+        return t;
     }
 
 }
