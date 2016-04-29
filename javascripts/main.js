44e38f0b9a26ddd745946245b231f4d1e615df15
John Elizarraras <18John.Elizarraras@boiseschools.net>
2016-04-29 13:03:59 -0600

    Does the beginnings of the compiler

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,59 @@
-console.log('This would be the main JS file.');
+var file = [];
+function  start(fileArray) {
+	file = fileArray;
+	compiles();
+	showErrors();
+}
+
+function compiles(){
+	for (var i = 0; i < file.length; i++) {
+		var line = file[i];
+		if(isCommand(line)){
+			switch(command(line).toLowerCase()){
+				case "run":
+				checkRun(line);
+				break;
+				case "loop":
+				checkLoop;
+				break;
+				case "display":
+				check
+			}
+		}
+	};
+}
+
+function isCommand(line){
+	String command = command(line);
+	var res = true;
+	switch(command.toLowerCase()){
+		case "loop":
+		break;
+		case "run":
+		break;
+		case "{":
+		break;
+		case "}":
+		break;
+		case "alias":
+		break;
+		default:
+		result = false;
+		break;
+	}
+	return res;
+}
+
+function command(line){
+	var command = "";
+	for (var i = 0; i < line.length; i +) {
+		var str = line.substring(i, i + 1);
+		if(!(str === " " || str === ".")){
+			command += str;
+		}
+		else{
+			break;
+		}
+	};
+	return command;
+}
