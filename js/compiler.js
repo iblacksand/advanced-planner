0dfb51f815475431287523125cad76a0c77a0b65
iblacksand <chess4j@gmail.com>
2016-05-11 18:59:06 -0600

    cleaning up

--- js/compiler.js
+++ js/compiler.js
@@ -1,5 +1,6 @@
 var file = [];
 var errors = [];
+
 function start() {
     errors = [];
     file = document.getElementById("code").value.split('\n');
@@ -8,7 +9,7 @@ function start() {
     showErrors();
 }
 
-function addError(line, message){
+function addError(line, message) {
     errors.push("Error on line: " + line + " Error Message: " + message);
     errors.push("---")
 }
@@ -16,30 +17,30 @@ function addError(line, message){
 /**
  * goes through the text field and checks each line
  */
-function compiles(){
+function compiles() {
     for (var i = 0; i < file.length; i++) {
         var line = file[i];
-        if(isCommand(i)){
-            switch(command(i).toLowerCase()){
-                case "run":
-                    break;
-                case "loop":
-                    checkLoop(i);
-                    break;
-                case "display":
-                    break;
-                case "{":
-                    checkOpenBrackets(i);
-                    break;
-                case "}":
-                    checkCloseBrackets(i);
-                    break;
+        if (isCommand(i)) {
+            switch (command(i).toLowerCase()) {
+            case "run":
+                break;
+            case "loop":
+                checkLoop(i);
+                break;
+            case "display":
+                break;
+            case "{":
+                checkOpenBrackets(i);
+                break;
+            case "}":
+                checkCloseBrackets(i);
+                break;
             }
         }
     };
 }
 
-function showErrors(){
+function showErrors() {
     document.getElementById("errors").value = "---";
     for (var index = 0; index < errors.length; index++) {
         var element = errors[index];
@@ -47,152 +48,144 @@ function showErrors(){
     }
 }
 
-function lineContains(want, toCheck, p, q){
-    try{
+function lineContains(want, toCheck, p, q) {
+    try {
         return want == toCheck.substring(p, q);
-    }
-    catch(err){}
+    } catch (err) {}
     return false;
 }
 
-function checkOpenBrackets(k){
-    try{
-        if(!lineContains("loop", file[k - 1], 0, 4)) addError(k + 1, "Opend bracket without loop before");
-    }
-    catch(err){
+function checkOpenBrackets(k) {
+    try {
+        if (!lineContains("loop", file[k - 1], 0, 4)) addError(k + 1, "Opend bracket without loop before");
+    } catch (err) {
         addError(k + 1, "Open bracket without loop before");
     }
 }
 
-function checkCloseBrackets(k){
-    try{
+function checkCloseBrackets(k) {
+    try {
         var needError = true;
-        for(var i = k - 1; i >= 0; i--){
-            if(lineContains("loop", file[i], 0, 4)){
+        for (var i = k - 1; i >= 0; i--) {
+            if (lineContains("loop", file[i], 0, 4)) {
                 needError = false;
                 break;
-            }
-            else if(lineContains("}", file[i], 0, 4)) addError(i + 1, "'}' before '{'");
+            } else if (lineContains("}", file[i], 0, 4)) addError(i + 1, "'}' before '{'");
         }
-        if(needError) addError(index, "'}' with no opening bracket");
-    }
-    catch(err){
+        if (needError) addError(index, "'}' with no opening bracket");
+    } catch (err) {
         addError(k + 1, "Closed bracket in illegal position");
     }
 }
 
-function checkLoop(lineNum){
+function checkLoop(lineNum) {
     var mods = this.object(lineNum);
-    if(mods.length < 2) addError(lineNum + 1, "Not Enough Modifiers");
-    else if(mods.length > 2) addError(lineNum + 1, "Too Many Modifiers");
-    else{
-        if(Number.isNaN(mods[0].substring(0, mods[0].length - 1))) addError(lineNum + 1, "Time is not a number");
-        switch(mods[0].substring(mods[0].length - 1)){
-            case "s":
-                break;
-            case "m":
-                break;
-            case "h":
-                break;
-            case "d":
-                break;
-            default:
-                addError(lineNum + 1, "Not an acceptable time format");
-                break;
+    if (mods.length < 2) addError(lineNum + 1, "Not Enough Modifiers");
+    else if (mods.length > 2) addError(lineNum + 1, "Too Many Modifiers");
+    else {
+        if (Number.isNaN(mods[0].substring(0, mods[0].length - 1))) addError(lineNum + 1, "Time is not a number");
+        switch (mods[0].substring(mods[0].length - 1)) {
+        case "s":
+            break;
+        case "m":
+            break;
+        case "h":
+            break;
+        case "d":
+            break;
+        default:
+            addError(lineNum + 1, "Not an acceptable time format");
+            break;
         }
-        if(Number.isNaN(mods[1])) addError(lineNum + 1, "Second modifier is not a number");
-        else{
-            if(mods[1] < 0) addError(lineNum + 1, "Second modifier is negative");
+        if (Number.isNaN(mods[1])) addError(lineNum + 1, "Second modifier is not a number");
+        else {
+            if (mods[1] < 0) addError(lineNum + 1, "Second modifier is negative");
         }
-        try{
-            if(!(file[lineNum + 1] == "{")) addError(lineNum + 2, "Is not a '{'");
+        try {
+            if (!(file[lineNum + 1] == "{")) addError(lineNum + 2, "Is not a '{'");
             var needsError = true;
-            for(var i = lineNum + 2; i < file.length; i++){
-                if(file[i] == "{") addError(i + 1, "Another '{' before '}'");
-                else if(file[i] == "}"){
+            for (var i = lineNum + 2; i < file.length; i++) {
+                if (file[i] == "{") addError(i + 1, "Another '{' before '}'");
+                else if (file[i] == "}") {
                     needsError = false;
                     break;
                 }
             }
-            if(needsError) addError(file.length + 1, "No Closing '}'");
-        }
-        catch(err){
+            if (needsError) addError(file.length + 1, "No Closing '}'");
+        } catch (err) {
             addError(file.length + 1, "Loop doesn't open/close");
         }
     }
 }
 
-function object(line){
-    if(line < file.length && line > -1){
+function object(line) {
+    if (line < file.length && line > -1) {
         var a = [];
         var curLine = file[line];
         var startFound = false;
         var start = 0;
-        for(var i = 0; i < curLine.length; i++){
-            if(curLine.substring(i, i+1) === " "){
+        for (var i = 0; i < curLine.length; i++) {
+            if (curLine.substring(i, i + 1) === " ") {
                 start = i;
                 startFound = true;
                 break;
             }
         }
-        if(!startFound) return [];
+        if (!startFound) return [];
         var str = curLine.substring(start + 1);
         var build = "";
         var lastCut = 0;
-        for(var i = 0; i < str.length; i++){
+        for (var i = 0; i < str.length; i++) {
             var c = str.substring(i, i + 1);
-            if(c === " "){
+            if (c === " ") {
                 a.push(build.trim());
                 build = "";
                 lastCut = i;
-            }
-            else build += c.trim();
+            } else build += c.trim();
         }
         a.push(str.substring(lastCut).trim());
         return a;
 
-    }
-    else return [];
+    } else return [];
 }
 
-function isCommand(line){
+function isCommand(line) {
     var command = this.command(line);
     var res = true;
-    switch(command.toLowerCase()){
-        case "loop":
-            break;
-        case "run":
-            break;
-        case "{":
-            break;
-        case "}":
-            break;
-        case "alias":
-            break;
-        case "display":
-            break;
-        default:
-            addError(line + 1, "This is not a command");
-            result = false;
-            break;
+    switch (command.toLowerCase()) {
+    case "loop":
+        break;
+    case "run":
+        break;
+    case "{":
+        break;
+    case "}":
+        break;
+    case "alias":
+        break;
+    case "display":
+        break;
+    default:
+        addError(line + 1, "This is not a command");
+        result = false;
+        break;
     }
     return res;
 }
 
-function command(line){
+function command(line) {
     var command = "";
-    for (var i = 0; i < file[line].length; i ++) {
+    for (var i = 0; i < file[line].length; i++) {
         var str = file[line].substring(i, i + 1);
-        if(!(str === " " || str === ".")){
+        if (!(str === " " || str === ".")) {
             command += str;
-        }
-        else{
+        } else {
             break;
         }
     };
     return command;
 }
 
-function pause(){
+function pause() {
     return "";
 }
